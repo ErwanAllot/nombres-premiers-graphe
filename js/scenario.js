1f030fa6d1b024ce765a4524a2135f4060426177
AllotErwan <erwanallot@gmail.com>
2026-09-20 11:21:06 +0200

    simplification fonctions scénario

--- js/scenario.js
+++ js/scenario.js
@@ -1,8 +1,27 @@
+// --- RACCOURCIS MÉTIER POUR LE SCÉNARIO ---
+const ep = (epingles, val) => epingles.find(e => e.valeur === val);
+
+const ajouterCheminEntre = (epingles, chemins, v1, v2) => {
+    chemins.push(calculerCheminEtJoint(ep(epingles, v1), ep(epingles, v2)));
+};
+
+const allongerQueues = (epingles, ...valeurs) => {
+    valeurs.forEach(v => {
+        const p = ep(epingles, v);
+        if (p) p.longueurQueue += 1;
+    });
+};
+
+const nouvelleEpingle = (epingles, val, x, y, couleur = 'rouge') => {
+    epingles.push(creerEpingle(val, x, y, couleur));
+};
+
+
 // ==========================================
-// SCRIPT.JS - CONTRÔLEUR PRINCIPAL (Version Propre)
+// SCÉNARIO - LISTE DES ÉTAPES (Ultra-court)
 // ==========================================
 
-// --- INITIALISATION DU SCÉNARIO ---
+// --- ÉTAT INITIAL ---
 let listeEpinglesInitiales = [
     creerEpingle(3, -2, 0, 'violet'),
     creerEpingle(5, 2, 0, 'violet')
@@ -11,66 +30,47 @@ let listeEpinglesInitiales = [
 // ÉTAPE 1 : État de départ pur
 window.historiqueApp.enregistrerEtape("État de départ (3 et 5)", [...listeEpinglesInitiales], []);
 
-// ÉTAPE 2 : Premier chemin entre 3 et 5
+// ÉTAPE 2 : Premier chemin
 etape("Tracé du premier chemin vers J", (epingles, chemins) => {
-    const p3 = epingles.find(e => e.valeur === 3);
-    const p5 = epingles.find(e => e.valeur === 5);
-    chemins.push(calculerCheminEtJoint(p3, p5));
+    ajouterCheminEntre(epingles, chemins, 3, 5);
 });
 
-// ÉTAPE 3 : Génération de l'épingle 7
+// ÉTAPE 3 : Épingle 7
 etape("Génération de l'épingle 7 (Rouge)", (epingles) => {
-    epingles.push(creerEpingle(7, 0, 2, 'rouge'));
+    nouvelleEpingle(epingles, 7, 0, 2, 'rouge');
 });
 
-// ÉTAPE 4 : Allongement des queues de 3 et 5
+// ÉTAPE 4 : Allongement 3 et 5
 etape("Allongement des queues de 3 et 5", (epingles) => {
-    epingles.find(e => e.valeur === 3).longueurQueue += 1;
-    // const p3 = epingles.find(e => e.valeur === 3);
-    const p5 = epingles.find(e => e.valeur === 5);
-    // if (p3) p3.longueurQueue += 1;
-    if (p5) p5.longueurQueue += 1;
+    allongerQueues(epingles, 3, 5);
 });
 
-// ÉTAPE 5 : Création du chemin (3, 7)
+// ÉTAPE 5 : Chemin (3, 7)
 etape("Test unique du chemin (3, 7)", (epingles, chemins) => {
-    const p3 = epingles.find(e => e.valeur === 3);
-    const p7 = epingles.find(e => e.valeur === 7);
-    chemins.push(calculerCheminEtJoint(p3, p7));
+    ajouterCheminEntre(epingles, chemins, 3, 7);
 });
 
-// ÉTAPE 6 : Génération de l'épingle 11
+// ÉTAPE 6 : Épingle 11
 etape("Génération de l'épingle 11 (Rouge)", (epingles) => {
-    epingles.push(creerEpingle(11, -5, 3, 'rouge'));
+    nouvelleEpingle(epingles, 11, -5, 3, 'rouge');
 });
 
-// ÉTAPE 7 : Allongement des queues de 3 et 7
+// ÉTAPE 7 : Allongement 3 et 7
 etape("Allongement des queues de 3 et 7", (epingles) => {
-    const p3 = epingles.find(e => e.valeur === 3);
-    const p7 = epingles.find(e => e.valeur === 7);
-    if (p3) p3.longueurQueue += 1;
-    if (p7) p7.longueurQueue += 1;
+    allongerQueues(epingles, 3, 7);
 });
 
-// ÉTAPE 8 : Création du chemin (5, 7)
+// ÉTAPE 8 : Chemin (5, 7)
 etape("Test unique du chemin (5, 7)", (epingles, chemins) => {
-    const p5 = epingles.find(e => e.valeur === 5);
-    const p7 = epingles.find(e => e.valeur === 7);
-    chemins.push(calculerCheminEtJoint(p5, p7));
+    ajouterCheminEntre(epingles, chemins, 5, 7);
 });
 
-// ÉTAPE 9 : Génération de l'épingle 13
+// ÉTAPE 9 : Épingle 13
 etape("Génération de l'épingle 13 (Rouge)", (epingles) => {
-    epingles.push(creerEpingle(13, 4, 5, 'rouge'));
+    nouvelleEpingle(epingles, 13, 4, 5, 'rouge');
 });
 
-// ÉTAPE 10 : Allongement des queues de 5 et 7
+// ÉTAPE 10 : Allongement 5 et 7
 etape("Allongement des queues de 5 et 7", (epingles) => {
-    const p5 = epingles.find(e => e.valeur === 5);
-    const p7 = epingles.find(e => e.valeur === 7);
-    if (p5) p5.longueurQueue += 1;
-    if (p7) p7.longueurQueue += 1;
+    allongerQueues(epingles, 5, 7);
 });
-
-
-
